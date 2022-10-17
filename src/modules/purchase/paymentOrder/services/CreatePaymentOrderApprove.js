5394e045a3ff9d229453327b91906214ef540d6c
M Adyan Rohutomo <muhadyan@Adyans-MacBook-Pro.local>
2022-10-18 05:07:57 +0700

    Enhance Approve Payment Order

--- src/modules/purchase/paymentOrder/services/CreatePaymentOrderApprove.js
+++ src/modules/purchase/paymentOrder/services/CreatePaymentOrderApprove.js
@@ -23,6 +23,7 @@ class CreatePaymentOrderApprove {
       return { paymentOrder };
     }
     await this.tenantDatabase.sequelize.transaction(async (transaction) => {
+      await updateJournal(this.tenantDatabase, { transaction, paymentOrder, form });
       await form.update({ approvalStatus: 1, approvalBy: this.approver.id, approvalAt: new Date() }, { transaction });
     });
 
@@ -39,6 +40,132 @@ function validate(form, approver) {
   if (form.requestApprovalTo !== approver.id) {
     throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden - You are not the selected approver');
   }
+}
+
+async function updateJournal(tenantDatabase, { transaction, paymentOrder, form }) {
+  await createJournalPaymentOrder(tenantDatabase, { transaction, paymentOrder, form });
+  await createJournalReturn(tenantDatabase, { transaction, paymentOrder, form });
+  await createJournalDownPayment(tenantDatabase, { transaction, paymentOrder, form });
+  await createJournalOther(tenantDatabase, { transaction, paymentOrder, form });
+  await createJournalInvoice(tenantDatabase, { transaction, paymentOrder, form });
+}
+
+async function createJournalPaymentOrder(tenantDatabase, { transaction, paymentOrder, form }) {
+  const settingJournal = await getSettingJournal(tenantDatabase, { feature: 'purchase', name: 'account payable' });
+  await tenantDatabase.Journal.create(
+    {
+      formId: form.id,
+      journalableType: 'Supplier',
+      journalableId: paymentOrder.supplierId,
+      chartOfAccountId: settingJournal.chartOfAccountId,
+      credit: paymentOrder.amount,
+    },
+    { transaction }
+  );
+}
+
+async function createJournalReturn(tenantDatabase, { transaction, paymentOrder, form }) {
+  if (parseInt(paymentOrder.totalReturn) === 0) {
+    return;
+  }
+
+  const settingJournal = await getSettingJournal(tenantDatabase, { feature: 'purchase', name: 'account payable' });
+  await tenantDatabase.Journal.create(
+    {
+      formId: form.id,
+      journalableType: 'Supplier',
+      journalableId: paymentOrder.supplierId,
+      chartOfAccountId: settingJournal.chartOfAccountId,
+      credit: paymentOrder.totalReturn,
+    },
+    { transaction }
+  );
+}
+
+async function createJournalDownPayment(tenantDatabase, { transaction, paymentOrder, form }) {
+  if (parseInt(paymentOrder.totalDownPayment) === 0) {
+    return;
+  }
+
+  const settingJournal = await getSettingJournal(tenantDatabase, { feature: 'purchase', name: 'down payment' });
+  await tenantDatabase.Journal.create(
+    {
+      formId: form.id,
+      journalableType: 'Supplier',
+      journalableId: paymentOrder.supplierId,
+      chartOfAccountId: settingJournal.chartOfAccountId,
+      credit: paymentOrder.totalDownPayment,
+    },
+    { transaction }
+  );
+}
+
+async function createJournalOther(tenantDatabase, { transaction, paymentOrder, form }) {
+  if (parseInt(paymentOrder.totalOther) === 0) {
+    return;
+  }
+
+  const others = await tenantDatabase.PaymentOrderOther.findAll({
+    where: { paymentOrderId: paymentOrder.id },
+    include: [{ model: tenantDatabase.ChartOfAccount, as: 'chartOfAccount' }],
+  });
+
+  for (const other of others) {
+    const coa = await tenantDatabase.ChartOfAccount.findOne({
+      where: { id: other.chartOfAccountId },
+      include: [{ model: tenantDatabase.ChartOfAccountType, as: 'type' }],
+    });
+
+    if (coa.type.name === 'DIRECT EXPENSE') {
+      await tenantDatabase.Journal.create(
+        {
+          formId: form.id,
+          chartOfAccountId: other.chartOfAccountId,
+          debit: other.amount,
+        },
+        { transaction }
+      );
+    }
+    if (coa.type.name === 'OTHER INCOME') {
+      await tenantDatabase.Journal.create(
+        {
+          formId: form.id,
+          chartOfAccountId: other.chartOfAccountId,
+          credit: other.amount,
+        },
+        { transaction }
+      );
+    }
+  }
+}
+
+async function createJournalInvoice(tenantDatabase, { transaction, paymentOrder, form }) {
+  const settingJournal = await getSettingJournal(tenantDatabase, { feature: 'purchase', name: 'account payable' });
+  await tenantDatabase.Journal.create(
+    {
+      formId: form.id,
+      journalableType: 'Supplier',
+      journalableId: paymentOrder.supplierId,
+      chartOfAccountId: settingJournal.chartOfAccountId,
+      debit: paymentOrder.totalInvoice,
+    },
+    { transaction }
+  );
+}
+
+async function getSettingJournal(tenantDatabase, { feature, name }) {
+  const settingJournal = await tenantDatabase.SettingJournal.findOne({
+    where: {
+      feature,
+      name,
+    },
+  });
+
+  if (!settingJournal) {
+    throw new ApiError(httpStatus.UNPROCESSABLE_ENTITY, `Journal ${feature} account - ${name} not found`);
+  }
+
+  return settingJournal;
 }
 
 module.exports = CreatePaymentOrderApprove;
